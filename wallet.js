8e5b4d0fee165a6ae8425d29f76591c5f783fbaa
Josh Harvey <josh@lamassu.is>
2016-11-25 16:59:36 +0200

    bit of a rewrite

--- wallet.js
+++ wallet.js
@@ -1,145 +1,94 @@
-var _ = require('lodash')
-var bitgolib = require('bitgo')
-var pjson = require('./package.json')
-var userAgent = 'Lamassu-BitGo/' + pjson.version
-var BigNumber = require('bignumber.js')
+const _ = require('lodash')
+const BitGo = require('bitgo')
+const pjson = require('./package.json')
+const userAgent = 'Lamassu-BitGo/' + pjson.version
 
-exports.NAME = 'BitGo'
-exports.SUPPORTED_MODULES = ['wallet']
+const NAME = 'BitGo'
+const SUPPORTED_MODULES = ['wallet']
 
-var pluginConfig = {
-  // token: <your BitGo access token>
-  // walletId: <your wallet id>,
-  // walletPassphrase: <your wallet passphrase>
-}
+let bitgo
+const pluginConfig = {}
 
-var bitgo
-
-var getWallet = function () {
+function getWallet () {
   return bitgo.wallets().get({ id: pluginConfig.walletId })
 }
 
-exports.config = function config (localConfig) {
+function config (localConfig) {
   if (localConfig) {
     _.merge(pluginConfig, localConfig)
   }
   if (pluginConfig.token && pluginConfig.walletId && pluginConfig.walletPassphrase) {
-    bitgo = new bitgolib.BitGo({ env: 'prod', userAgent: userAgent })
-    bitgo._token = pluginConfig.token
+    bitgo = new BitGo.BitGo({ accessToken: pluginConfig.token, env: 'prod', userAgent: userAgent })
   } else {
     throw new Error('BitGo config requires token and walletId')
   }
 }
 
-exports.sendCoins = function sendBitcoins (address, satoshis, fee, callback) {
+function sendCoins (address, satoshis, fee) {
   return getWallet()
-    .then(function (wallet) {
-      var params = {
-        address: address,
-        amount: satoshis,
-        // fee: fee,  // TODO: support fee in sendCoins
-        walletPassphrase: pluginConfig.walletPassphrase
-      }
-      return wallet.sendCoins(params)
-    })
-    .then(function (result) {
-      return result.hash
-    })
-    .catch(function (err) {
-      if (err.message === 'Insufficient funds') {
-        err.name = 'InsufficientFunds'
-      }
-      throw err
-    })
-    .nodeify(callback)
+  .then(wallet => {
+    const params = {
+      address: address,
+      amount: satoshis,
+      // fee: fee,  // TODO: support fee in sendCoins
+      walletPassphrase: pluginConfig.walletPassphrase
+    }
+    return wallet.sendCoins(params)
+  })
+  .then(result => {
+    return result.hash
+  })
+  .catch(err => {
+    if (err.message === 'Insufficient funds') {
+      err.name = 'InsufficientFunds'
+    }
+    throw err
+  })
 }
 
-exports.balance = function balance (callback) {
+function balance () {
   return getWallet()
-    .then(function (wallet) {
-      return {
-        BTC: wallet.spendableBalance()
-      }
-    })
-    .nodeify(callback)
+  .then(wallet => {
+    return {
+      BTC: wallet.wallet.spendableConfirmedBalance
+    }
+  })
 }
 
-exports.newAddress = function newAddress (info, callback) {
-  var address
-  var wallet
-
+function newAddress (info) {
   return getWallet()
-    .then(function (result) {
-      wallet = result
-      return wallet.createAddress()
-    })
-    .then(function (result) {
-      address = result.address
+  .then(wallet => {
+    return wallet.createAddress()
+    .then(result => {
+      const address = result.address
+
       // If a label was provided, set the label
       if (info.label) {
         return wallet.setLabel({ address: address, label: info.label })
+        .then(() => address)
       }
-    })
-    .then(function () {
+
       return address
     })
-    .nodeify(callback)
-}
-
-function compareTxs (a, b) {
-  if (a.instant && b.instant) return 0
-  if (a.instant) return -1
-  if (b.instant) return 1
-  if (!a.pending && !b.pending) return 0
-  if (!a.pending) return -1
-  if (!b.pending) return 1
-  return 0
-}
-
-// This new call uses promises. We're in the process of upgrading everything.
-exports.getStatus = function getStatus (toAddress, requested) {
-  return bitgo.blockchain().getAddressTransactions({address: toAddress})
-  .then(function (rec) {
-    var txs = rec.transactions
-    if (txs.length === 0) return {status: 'notSeen'}
-
-    return getWallet()
-    .then(function (wallet) {
-      var promises = txs.map(function (tx) {
-        return wallet.getTransaction({id: tx.id})
-        .then(function (walletTx) {
-          return {
-            entries: tx.entries,
-            instant: walletTx.instant,
-            pending: tx.pending
-          }
-        })
-      })
-
-      return Promise.all(promises)
-      .then(function (res) {
-        var sorted = res.sort(compareTxs)
-
-        var reduction = sorted.reduce(function (acc, tx) {
-          if (acc.total.gte(requested)) return acc
-
-          var entry = tx.entries.find(function (_entry) {
-            return _entry.account === toAddress
-          })
-
-          if (!entry || entry.value <= 0) return acc
-          var total = acc.total.plus(entry.value)
-
-          var status = tx.instant
-          ? 'instant'
-          : tx.pending ? 'authorized' : 'confirmed'
-
-          return ({total: total, status: status})
-        }, {total: new BigNumber(0), status: 'notSeen'})
-
-        if (reduction.total.lt(requested)) return {status: 'insufficientFunds'}
-        return {status: reduction.status}
-      })
-    })
   })
+}
+
+function getStatus (toAddress, requested) {
+  return bitgo.blockchain().getAddress({address: toAddress})
+  .then(rec => {
+    if (rec.balance === 0) return {status: 'notSeen'}
+    if (rec.balance < requested) return {status: 'insufficientFunds'}
+    if (rec.confirmedBalance < requested) return {status: 'authorized'}
+    return {status: 'confirmed'}
+  })
+}
+
+module.exports = {
+  NAME,
+  SUPPORTED_MODULES,
+  config,
+  balance,
+  sendCoins,
+  newAddress,
+  getStatus
 }
